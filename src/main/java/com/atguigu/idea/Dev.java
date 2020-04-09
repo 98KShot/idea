10da29814d5b097efb61dd19a338d1c9840bb59d
yry <jsyzyry@outlook.com>
2020-04-09 16:08:42 +0800

    jsyzyry修改fenzhi

--- src/main/java/com/atguigu/idea/Dev.java
+++ src/main/java/com/atguigu/idea/Dev.java
@@ -3,5 +3,8 @@ package com.atguigu.idea;
 public class Dev {
     public static void main(String[] args) {
         System.out.println("dev");
+        System.out.println("dev");
+        System.out.println("dev");
+        System.out.println("dev");
     }
 }
